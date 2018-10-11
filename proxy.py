87435a94440b7e7687eef68af523b79c656a410d
Matevz Vucnik <vucnikm@gmail.com>
2018-10-11 17:55:44 +0200

    add multi pmc support

--- proxy.py
+++ proxy.py
@@ -4,7 +4,7 @@ import json
 import paho.mqtt.client as mqtt
 
 url_sub = "193.2.205.66"
-pmcs = [("pmc/16700204541000610066a000a00000c1",0)]
+pmcs = [("pmc/1670020454100061004aa000a0000005",0),("pmc/16700204541000610066a000a00000c1",0)]
 
 url_pub = "localhost"
 mqtts = mqtt.Client()
@@ -12,7 +12,7 @@ mqtts.username_pw_set("user", "pass")
 mqtts.connect(url_pub)
 
 saam_msg = {}
-counters = {"16700204541000610066a000a00000c1": 0}
+counters = {"1670020454100061004aa000a0000005": 0, "16700204541000610066a000a00000c1": 0}
 
 def on_connect(mqttc, userdata, flags, rc):
     global pmcs
@@ -27,12 +27,12 @@ def on_message(mqttc, userdata, msg):
     node_id = data["node_id"]
 
     if counters[node_id] == 20:
-        sid = saam_msg["node_id"]
-        del saam_msg["node_id"]
+        sid = saam_msg[node_id]["node_id"]
+        del saam_msg[node_id]["node_id"]
         
-        for field in saam_msg:
-            mqtts.publish("saam_data/"+sid+"/"+field, json.dumps(saam_msg[field]))
-        saam_msg = {}
+        for field in saam_msg[node_id]:
+            mqtts.publish("saam_data/"+sid+"/"+field, json.dumps(saam_msg[node_id][field]))
+        del saam_msg[node_id]
         counters[node_id] = 0
     else:
         counters[node_id] = counters[node_id] + 1
@@ -45,14 +45,14 @@ def on_message(mqttc, userdata, msg):
     del data["input_2_status"]
     del data["input_3_status"]
 
-    if not saam_msg:
-        saam_msg = {"node_id": saam_id}
+    if node_id not in saam_msg:
+        saam_msg[node_id] = {"node_id": saam_id}
 
     for i,field in enumerate(data):
         source_id = field
-        if source_id not in saam_msg:
-            saam_msg[source_id] = {"timestamp": ts, "period": 500, "measurements": []}
-        saam_msg[source_id]["measurements"].append(float(data[field]))
+        if source_id not in saam_msg[node_id]:
+            saam_msg[node_id][source_id] = {"timestamp": ts, "period": 500, "measurements": []}
+        saam_msg[node_id][source_id]["measurements"].append(float(data[field]))
 
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
